c108d6247993be1974456f25b11df7198dda3f04
dlwnsfml <dlwnsfml@naver.com>
2025-09-08 19:54:44 +0900

    채팅방에 api 추가

--- src/main/java/com/ssafy/a705/domain/chat/controller/ChatRoomController.java
+++ src/main/java/com/ssafy/a705/domain/chat/controller/ChatRoomController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/v1/chat/rooms")
+@RequestMapping("/api/v1/chat/rooms")
 public class ChatRoomController {
 
     private final ChatRoomService roomService;
--- src/main/java/com/ssafy/a705/domain/chat/controller/ReadStatusController.java
+++ src/main/java/com/ssafy/a705/domain/chat/controller/ReadStatusController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/v1/chat")
+@RequestMapping("/api/v1/chat")
 public class ReadStatusController {
 
     private final ChatService chatService;
