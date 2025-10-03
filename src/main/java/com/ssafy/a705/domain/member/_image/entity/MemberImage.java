b62e314feba7db23f89f7223e948efe474b0fd4a
leedy903 <leedy903@naver.com>
2025-10-03 17:23:37 +0900

    feat: ERD에 추가된 Entity 생성
    > MemberImage: Member의 프로필 이미지 Entity
    > Invitation: 그룹 초대 Entity

---
+++ src/main/java/com/ssafy/a705/domain/member/_image/entity/MemberImage.java
@@ -0,0 +1,41 @@
+package com.ssafy.a705.domain.member._image.entity;
+
+import com.ssafy.a705.domain.member.entity.Member;
+import com.ssafy.a705.global.common.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Comment;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class MemberImage extends BaseEntity {
+
+    @Id
+    @Comment("멤버 프로필 이미지 식별자")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Comment("이미지 url")
+    @Column(nullable = false)
+    private String url;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    private MemberImage(String url, Member member) {
+        this.url = url;
+        this.member = member;
+    }
+
+}
